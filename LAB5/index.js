cc84047ea747fc0f25e1c2499ae490d71e735d22
2017123126 <shj05230@yonsei.ac.kr>
2022-06-01 07:29:13 +0900

    LAB5

--- LAB5/index.js
+++ LAB5/index.js
@@ -1,11 +1,17 @@
-fetch('product.json')
-  .then( response => {
-    if (!response.ok) {
-      alert(`HTTP error: ${response.status}`);
-    }
-    alert("success");
-    return response.json();
-  })
-  .then( json => initialize(json) )
-  .catch( err => alert(`Fetch problem: ${err.message}`) );
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById("search_button").addEventListener('click', makeRequest);
+});
 
+function makeRequest(){
+    fetch("product.json")
+    .then( response => {
+        if (!response.ok) {
+          alert(`HTTP error: ${response.status}`);
+        }
+        alert("Success");
+        return response.json();
+      })
+      .then( json => initialize(json) )
+      .catch( err => alert(`Fetch problem: ${err.message}`) );
+    
+}
